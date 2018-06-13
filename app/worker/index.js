3a9259f793722be3470c0b43915422915d67dfdb
Raul Rosenlöcher <git@home>
2018-06-13 12:49:29 +0200

    renamed worker entry: worker.js => index.js

---
+++ app/worker/index.js
@@ -0,0 +1,13 @@
+import log from 'electron-log';
+
+
+console.log("con - worker.js - loaded");
+//alert("Hello! I am an alert box!!");
+
+//const log = require('electron-log');
+log.info("log - worker.js - loaded");
+
+//import log from 'electron-log';
+//log.info("worker.js - loaded");
+
+
